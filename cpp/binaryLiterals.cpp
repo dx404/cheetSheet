08a6ddfdeda8e97ec10439bc7f60191dff629069
Duo "Donald" Zhao <wisdom.to.pilot@gmail.com>
2014-11-17 14:43:54 -0500

    Create binaryLiterals.cpp

---
+++ cpp/binaryLiterals.cpp
@@ -0,0 +1,21 @@
+/**
+ *  g++     -std=c++1y binaryLiterals.cpp 
+ *  clang++ -std=c++1y binaryLiterals.cpp 
+ *
+ */
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	vector<int> num = {
+		0b11'111, 0b11'111, // binary, digit separators
+		037,				        // octary 
+		31,					        // decimal
+		0x1f, 0x1f			    // hexadecimal 	
+	};
+	for (auto x : num) {
+		cout << x << endl;  // decimal output
+	}
+	return 0;
+}
