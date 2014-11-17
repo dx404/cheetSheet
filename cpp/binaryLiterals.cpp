29470e7c7afe869e2bc81926c85d955eb2865e3f
Duo "Donald" Zhao <wisdom.to.pilot@gmail.com>
2014-11-17 14:44:25 -0500

    Update binaryLiterals.cpp

--- cpp/binaryLiterals.cpp
+++ cpp/binaryLiterals.cpp
@@ -10,9 +10,9 @@ using namespace std;
 int main(int argc, char *argv[]) {
 	vector<int> num = {
 		0b11'111, 0b11'111, // binary, digit separators
-		037,				        // octary 
-		31,					        // decimal
-		0x1f, 0x1f			    // hexadecimal 	
+		037,		    // octary 
+		31,		    // decimal
+		0x1f, 0x1f	    // hexadecimal 	
 	};
 	for (auto x : num) {
 		cout << x << endl;  // decimal output
